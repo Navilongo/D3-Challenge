1b13b411c9f9aa240bec6d628c4deeb70f8b4555
Navilongo <navilongo@gmail.com>
2021-05-31 12:11:49 -0700

    updated svg group

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -9,10 +9,16 @@ var margin = {
     left: 50
 };
 
+// setting up borders
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+// Crating the svg group where the chart will rest
 var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
+
+// Append SVG group
+const chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
